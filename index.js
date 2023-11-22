01aef7817b70101027a07dccb3c8672d1ee3282a
Justin Sterling <justinsterling44@gmail.com>
2023-11-22 17:33:36 -0500

    Add project code

---
+++ index.js
@@ -0,0 +1,59 @@
+// index.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint... 
+app.get("/api/:date?", function (req, res) {
+  var date;
+  var now_utc;
+  if (!req.params.date) {
+    date = new Date();
+    now_utc = Date.UTC(date.getUTCFullYear(), date.getUTCMonth(),
+      date.getUTCDate(), date.getUTCHours(),
+      date.getUTCMinutes(), date.getUTCSeconds());
+    res.json({unix: now_utc, utc: date.toUTCString()});
+  }
+  else {
+    try {
+      if (/(^[\d]{13}$)/.test(req.params.date)) {
+        date = new Date(+req.params.date);
+      } else{
+        date = new Date(req.params.date);
+      }
+      if (date == "Invalid Date") res.json({error: "Invalid Date"});
+      now_utc = Date.UTC(date.getUTCFullYear(), date.getUTCMonth(),
+        date.getUTCDate(), date.getUTCHours(),
+        date.getUTCMinutes(), date.getUTCSeconds());
+      res.json({unix: now_utc, utc: date.toUTCString()});
+    }
+    catch(err) {
+      res.json({error: "Invalid Date"});
+    }
+    
+  }
+  
+});
+
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
